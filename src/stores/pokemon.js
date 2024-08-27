dcab969e69a98f365ddac8823fd3f4fe639ac9a8
DavidGalileo24 <david.salgado.mejia@dive.digital>
2024-08-27 11:38:46 -0600

    Working in card

--- src/stores/pokemon.js
+++ src/stores/pokemon.js
@@ -4,43 +4,56 @@ import axios from "axios";
 export const usePokemonStore = defineStore("pokemon", {
   state: () => ({
     statusCard: false,
+    image: '../../public/images/8.png', 
     firstArray: [
       {
         id: 1,
         name: "Hormiga",
+        statusInCard: false,
         file: "../../public/images/1.png",
       },
       {
         id: 2,
         name: "Moneda",
+        statusInCard: false,
         file: "../../public/images/2.png",
       },
       {
         id: 3,
         name: "Araña",
+        statusInCard: false,
         file: "../../public/images/4.png",
       },
       {
         id: 4,
         name: "Duende",
+        statusInCard: false,
         file: "../../public/images/6.png",
       },
       /**
           {
             id: 5,
-            file: '../../public/images/5.png'
+            name: "Duende",
+            statusInCard: false,
+            file: '../../public/images/5.png',
           },
           {
             id: 6,
-            file: '../../public/images/6.png'
+            name: "Duende",
+            statusInCard: false,
+            file: '../../public/images/6.png',
           },
           {
             id: 7,
-            file: '../../public/images/7.png'
+            name: "Duende",
+            statusInCard: false,
+            file: '../../public/images/7.png',
           },
           {
             id: 8,
-            file: '../../public/images/8.png'
+            name: "Duende",
+            statusInCard: false,
+            file: '../../public/images/8.png',
           }, */
     ],
   }),
@@ -52,9 +65,15 @@ export const usePokemonStore = defineStore("pokemon", {
                 console.log(e);
             });
         } */
-    getImage(id) {
-      console.log(id);
-      this.statusCard = true;
+    getImage(data) {
+      console.log(data);
+      this.defaultImage = false;
+      data.statusInCard = true
+      if(data.statusInCard == false) {
+        this.image = this.defaultImage;
+      } else { 
+        this.image = data.file;
+      }
     },
   },
 });
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -8,14 +8,14 @@ const secondArray = pokeData.firstArray.slice();
 </script>
 
 <template>
-  <div class="grid gap-3 mt-7 grid-cols-4 w-2/3 mx-auto">
-    <div v-for="imag in pokeData.firstArray.concat(secondArray)" :key="imag.id" class="border rounded-md p-5 hover:cursor-pointer card">
-      <img src="../../public/images/8.png" class="w-24 h-24 mx-auto" @click="pokeData.getImage(imag.id)" v-if="pokeData.statusCard==false"/>
-      <img :src="imag.file" class="w-24 h-24 mx-auto" v-else />
+  <h1 class="text-center text-3xl font-semibold mt-10">Memory Game</h1>
+  <div class="grid gap-3 mt-7 grid-cols-4 w-2/3 mx-auto ">
+    <div v-for="imag in pokeData.firstArray" :key="imag.id" @click="pokeData.getImage(imag)" class="rounded-lg p-5 border-4 bg-gray-900 border-blue-300 hover:cursor-pointer card">
+      <img :src="pokeData.image" class="w-24 h-24 mx-auto" />
+      {{pokeData.image}}
       <!--<h3 class="text-center text-lg mt-2 font-semibold">{{ imag.name }}</h3>-->
     </div>
   </div>
 </template>
 <style scoped>
-
 </style>
